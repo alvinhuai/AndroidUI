f25e9392e61a05446f319b15ec5efa609d3bfd07
lichengfeng <li.chengfeng@nbbsw.com>
2016-04-13 21:13:50 +0800

    Android 通过暗码启动APP

---
+++ AndroidUI/src/com/lichengf/androidui/MainActivityReceiver.java
@@ -0,0 +1,37 @@
+package com.lichengf.androidui;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+
+public class MainActivityReceiver extends BroadcastReceiver {
+	
+	private static final String TAG = "MainActivityReceiver";
+
+	private static final String SECRET_CODE_ACTION = "android.provider.Telephony.SECRET_CODE";
+	private final Uri mEmUri = Uri.parse("android_secret_code://7758521");
+	@Override
+	public void onReceive(Context context, Intent intent) {
+        if (intent.getAction() == null) {
+            Log.i(TAG, "Null action");
+            return;
+        }
+        if (intent.getAction().equals(SECRET_CODE_ACTION)) {
+            Uri uri = intent.getData();
+            Log.i(TAG, "getIntent success in if");
+            if (uri.equals(mEmUri)) {
+                Intent intentEm = new Intent(context, MainActivity.class);
+                intentEm.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                Log.i(TAG, "Before start EM activity");
+                context.startActivity(intentEm);
+            } else {
+                Log.i(TAG, "No matched URI!");
+            }
+        } else {
+            Log.i(TAG, "Not SECRET_CODE_ACTION!");
+        }
+	}
+
+}
